439d8f0d8620daa9cbef2fe03a3e0a409ffb62fc
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-12-14 18:59:17 +0700

    Update buttonFlower.jsx

--- src/components/products/buttonFlower.jsx
+++ src/components/products/buttonFlower.jsx
@@ -108,7 +108,7 @@ export default function ButtonFlower({ cartItem }) {
         className={`fixed 
            ${
              isOpen
-               ? "opacity-100 -translate-y-[115%] max-custom:-translate-y-[270%]  "
+               ? "opacity-100 -translate-y-[105%] max-custom:-translate-y-[260%]  "
                : "opacity-0 -translate-y-[400%]"
            } w-full h-[400px] transition-transform duration-700  z-40 left-0 items-center flex justify-center
             }`}
