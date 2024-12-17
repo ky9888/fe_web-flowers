ff1a505b7e6ff99daa37260dd6fa7a5029c2e9b9
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-12-17 18:23:26 +0700

    Update page.jsx

--- src/app/san-pham/[name]/page.jsx
+++ src/app/san-pham/[name]/page.jsx
@@ -1,23 +1,28 @@
 import ProductsID from "@/components/products/productsID";
 
 export default async function Home({ params }) {
-  const { name } = await params; 
+  const { name } = params;
   const revertedString = name.replace(/-/g, " ");
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
-  );
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
+    );
 
-  if (!res.ok) {
-    return <div>Please enter a valid product ID or product not found.</div>;
+    if (!res.ok) {
+      return <div>Please enter a valid product ID or product not found.</div>;
+    }
+
+    const ress = await res.json();
+    const data = ress.data;
+
+    return (
+      <>
+        <ProductsID data={data} />
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return <div>Something went wrong. Please try again later.</div>;
   }
-
-  const ress = await res.json();
-  const data = ress.data;
-
-  return (
-    <>
-      <ProductsID data={data} />
-    </>
-  );
 }
