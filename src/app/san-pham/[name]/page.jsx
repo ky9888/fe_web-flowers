877ad4a3729205f15097dad16c358d085b66882a
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-12-20 19:29:41 +0700

    Update page.jsx

--- src/app/san-pham/[name]/page.jsx
+++ src/app/san-pham/[name]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import ProductsID from "@/components/products/productsID";
-
+import { AiOutlineReload } from "react-icons/ai";
 export default function Home({ params: paramsPromise }) {
   const [params, setParams] = useState(null);
   const [data, setData] = useState(null);
@@ -40,7 +40,12 @@ export default function Home({ params: paramsPromise }) {
   }, [params]);
 
   if (!data) {
-    return <div>No product data available.</div>;
+    return <div
+          className="fixed top-0 bg-white bottom-0
+        right-0 left-0 flex justify-center items-center "
+        >
+          <AiOutlineReload className="h-[10%] w-[10%] animate-spin text-green-700"  />
+        </div>;
   }
 
   return (
