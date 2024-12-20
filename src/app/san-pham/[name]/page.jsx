319a51980b7a0e02c2355c299be1b31e2e70fb84
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-12-20 19:35:27 +0700

    Update page.jsx

--- src/app/san-pham/[name]/page.jsx
+++ src/app/san-pham/[name]/page.jsx
@@ -1,56 +1,28 @@
-"use client";
-import { useEffect, useState } from "react";
 import ProductsID from "@/components/products/productsID";
-import { AiOutlineReload } from "react-icons/ai";
-export default function Home({ params: paramsPromise }) {
-  const [params, setParams] = useState(null);
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-   
-    const resolveParams = async () => {
-      const resolvedParams = await paramsPromise;
-      setParams(resolvedParams);
-    };
+export default async function Home({ params }) {
+  const { name } = params;
+  const revertedString = name.replace(/-/g, " ");
 
-    resolveParams();
-  }, [paramsPromise]);
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
+    );
 
-  useEffect(() => {
-    if (!params) return;
+    if (!res.ok) {
+      return <div>Please enter a valid product ID or product not found.</div>;
+    }
 
-    const revertedString = params.name.replace(/-/g, " ");
+    const ress = await res.json();
+    const data = ress.data;
 
-    const fetchData = async () => {
-    
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
-        );
-
-        if (!res.ok) {
-          throw new Error("Product not found or invalid product ID.");
-        }
-
-        const ress = await res.json();
-        setData(ress.data);
-      
-    };
-
-    fetchData();
-  }, [params]);
-
-  if (!data) {
-    return <div
-          className="fixed top-0 bg-white bottom-0
-        right-0 left-0 flex justify-center items-center "
-        >
-          <AiOutlineReload className="h-[10%] w-[10%] animate-spin text-green-700"  />
-        </div>;
+    return (
+      <>
+        <ProductsID data={data} />
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return <div>Something went wrong. Please try again later.</div>;
   }
-
-  return (
-    <>
-      <ProductsID data={data} />
-    </>
-  );
 }
