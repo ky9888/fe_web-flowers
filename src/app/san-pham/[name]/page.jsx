846d4d1a6b73a00691377577a3b6074afdec8b6d
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-12-22 15:15:37 +0700

    Update page.jsx

--- src/app/san-pham/[name]/page.jsx
+++ src/app/san-pham/[name]/page.jsx
@@ -4,25 +4,20 @@ export default async function Home({ params }) {
   const { name } = params;
   const revertedString = name.replace(/-/g, " ");
 
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
-    );
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
+  );
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch product');
-    }
-
-    const ress = await res.json();
-    const data = ress.data;
-
-    return (
-      <>
-        <ProductsID data={data} />
-      </>
-    );
-  } catch (error) {
-    console.error("Error fetching product details:", error);
-    return <div>Something went wrong. Please try again later.</div>;
+  if (!res.ok) {
+    throw new Error("Failed to fetch product");
   }
+
+  const ress = await res.json();
+  const data = ress.data;
+
+  return (
+    <>
+      <ProductsID data={data} />
+    </>
+  );
 }
