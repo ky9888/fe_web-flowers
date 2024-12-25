d501fa215793f5af246d2e46d8ae7e229b8c879c
ky9888 <135335702+ky9888@users.noreply.github.com>
2024-12-25 09:57:48 +0700

    Update page.jsx

--- src/app/san-pham/[name]/page.jsx
+++ src/app/san-pham/[name]/page.jsx
@@ -1,15 +1,57 @@
 import ProductsID from "@/components/products/productsID";
 
-export default async function Home({ params }) {
+export async function generateStaticParams() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL_API}/api/products/getAllProducts`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products list");
+  }
+
+  const ress = await res.json();
+  const products = ress.data;
+
+  const mappedProducts = products.map((product) => ({
+    name: product.name,
+  }));
+
+  return mappedProducts;
+}
+
+export async function generateMetadata({ params }) {
+  const { name } = params;
+  const decoded = decodeURIComponent(name).replace(/-/g, " ");
+
+  const revertedString = name.replace(/-/g, " ");
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch product metadata");
+  }
+
+  const ress = await res.json();
+  const data = ress.data;
+
+  return {
+    title:  decoded,
+    description: data.description 
+  };
+}
+
+export default async function ProductPage({ params }) {
   const { name } = params;
   const revertedString = name.replace(/-/g, " ");
 
   const res = await fetch(
-    `https://be-web-flowers.onrender.com/api/products/getDetailProducts/${revertedString}`
+    `${process.env.NEXT_PUBLIC_URL_API}/api/products/getDetailProducts/${revertedString}`
   );
 
   if (!res.ok) {
-    throw new Error("Failed to fetch product");
+    throw new Error("Failed to fetch product details");
   }
 
   const ress = await res.json();
